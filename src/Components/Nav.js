43ccf93cd55cebe3282a717eb3d1939f54039a47
Aidas <aidas19970711@gmail.com>
2021-11-01 15:48:04 +0200

    fixed href

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -5,33 +5,33 @@ const Nav = () => {
   return (
     <nav className={classes.navbar}>
       <div className={classes.navbar__div}>
-        <a className={classes.navbar__item} href="/">
+        <a className={classes.navbar__item} href="/coinbase/">
           <CoingateLogo />
         </a>
         <div className={classes.navbar__item}>
-          <a className={classes.navbar__link} href="/">
+          <a className={classes.navbar__link} href="/coinbase/">
             Products
           </a>
         </div>
         <div className={classes.navbar__item}>
-          <a className={classes.navbar__link} href="/">
+          <a className={classes.navbar__link} href="/coinbase/">
             Resources
           </a>
         </div>
         <div className={classes.navbar__item}>
-          <a className={classes.navbar__link} href="/">
+          <a className={classes.navbar__link} href="/coinbase/">
             Buy Instantly
           </a>
         </div>
       </div>
       <div className={classes.navbar__div}>
         <div className={classes.navbar__item}>
-          <a className={classes.navbar__link} href="/">
+          <a className={classes.navbar__link} href="/coinbase/">
             Log In
           </a>
         </div>
         <div className={classes.navbar__item + " " + classes.navbar__signup}>
-          <a className={classes.navbar__link} href="/">
+          <a className={classes.navbar__link} href="/coinbase/">
             Sign Up &gt;
           </a>
         </div>
